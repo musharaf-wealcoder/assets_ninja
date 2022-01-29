9c926f83b36336bbe96c318c9b17fd9bce10c337
Musharaf Hossain <musharaf.wealcoder@gmail.com>
2022-01-29 12:07:18 +0600

    enqueue scripts add

--- assets-ninja.php
+++ assets-ninja.php
@@ -9,16 +9,28 @@
    
 */
 
+define('ASSETS_DIR', plugin_dir_url(__FILE__). "assets/");
+
+
 class Assets_ninja{
+  
+  private $version;
 
   public function __construct()
   {
-    add_action('admin_init', array($this,'assets_textdomain_loade'));
+    $this->version = time();
+    add_action('plugin_loaded', array($this,'assets_textdomain_loade'));
+    add_action('wp_enqueue_scripts', array($this, 'assets_ninja_assets_enqueue'));
   }
 
   public function assets_textdomain_loade()
   {
     load_plugin_textdomain('assets_textdomain', false, plugin_dir_url(__FILE__), '/language');
+  }
+
+  public function assets_ninja_assets_enqueue()
+  {
+    wp_enqueue_script('assets_ninja_main_js', ASSETS_DIR.'admin/js/main.js', array('jquery'), $this->version, true);
   }
 
 
---
+++ assets/admin/js/main.js
@@ -0,0 +1,3 @@
+; (function($){
+  alert('hello file, are you main js file, ans: yes I am main js file');
+})(jQuery);
